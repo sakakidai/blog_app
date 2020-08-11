e97b714e7555ed42d28c8393542c2af1b6f95422
榊大 <sd.lybd1016@gmail.com>
2020-08-11 09:22:05 +0900

    create sample hello_vue

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -6,16 +6,14 @@
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
 import Vue from 'vue'
-import App from '../app.vue'
+import App from '../components/app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
 
-  console.log(app)
+new Vue({
+  el: '#hello',
+  render: h => h(App)
 })
+
 
 
 // The above code uses Vue without the compiler, which means you cannot
